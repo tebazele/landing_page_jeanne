8ee69c53dc7e904242ecd60cd3ca3ed005964b73
Jeanne Allen <tebazele@gmail.com>
2023-01-29 22:27:50 -0700

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-dark">
+<h1> Hello World</h1>
+    </div>
+</template>
+
+
+<script>
+
+import { computed, reactive, onMounted } from 'vue';
+export default {
+    setup(){
+    return {  }
+    }
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
